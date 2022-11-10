d2d0211fe50744f74eac69c555313f14f4eca67e
Artem Mokhov <teumik@gmail.com>
2022-11-11 00:17:52 +0400

    feat: add readline version of task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,45 @@
+// const fs = require('fs');
+// const { stdin, stdout } = process;
+// const path = require('path');
+// const filePath = path.join(__dirname, 'text.txt');
+// const output = fs.createWriteStream(filePath, 'utf-8');
+
+// stdout.write('Enter some text or word: (to see the changes, reopen text.txt)\n');
+
+// stdin.on('data', data => {
+//   const input = data.toString().trim();
+//   if (input === 'exit') {
+//     stdout.write('Edited is over, Bye!\n');
+//     process.exit();
+//   }
+//   output.write(data);
+// });
+
+// process.on('SIGINT', () => {
+//   stdout.write('\nEdited is over, Bye!\n');
+//   process.exit();
+// });
+
+const { stdin: input, stdout: output } = process;
 const fs = require('fs');
-const { stdin, stdout } = process;
 const path = require('path');
-const filePath = path.join(__dirname, 'text.txt');
-const output = fs.createWriteStream(filePath, 'utf-8');
+const textFile = path.join(__dirname, 'text.txt');
+const readline = require('readline');
+const rl = readline.createInterface({ input, output });
+const writeStream = fs.createWriteStream(textFile, 'utf-8');
 
-stdout.write('Enter some text or word: (to see the changes, reopen text.txt)\n');
+rl.write('Enter some text or word:\n');
 
-stdin.on('data', data => {
-  const input = data.toString().trim();
-  if (input === 'exit') {
-    stdout.write('Edited is over, Bye!\n');
-    process.exit();
+rl.on('line', (data) => {
+  if (data === 'exit') {
+    output.write('Edited is over, Bye!\n');
+    rl.close();
+  } else {
+    writeStream.write(data + '\n');
   }
-  output.write(data);
 });
 
-process.on('SIGINT', () => {
-  stdout.write('\nEdited is over, Bye!\n');
-  process.exit();
+rl.on('SIGINT', () => {
+  output.write('Edited is over, Bye!\n');
+  rl.close();
 });
